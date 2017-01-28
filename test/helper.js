44bde21de31424e9b30d1dc42e7bfe6251bf02fc
Marcos J. Alvarez <garusis@gmail.com>
2017-01-28 14:21:57 -0500

    Add useful utilities and test helper

---
+++ test/helper.js
@@ -0,0 +1,22 @@
+/**
+ * Created by garusis on 04/12/16.
+ */
+const chai = require('chai');
+const _ = require('lodash');
+const Promise = require('bluebird');
+const moment = require('moment');
+
+global.should = chai.should();
+global.expect = chai.expect;
+global.assert = chai.assert;
+global.BPromise = Promise;
+global.moment = moment;
+global._ = _;
+
+global.MathUtils = {
+  randomNumber: function (max, min) {
+    min = min || 0
+    max = max || 10
+    return min + Math.floor(Math.random() * (max - min + 1))
+  }
+}
